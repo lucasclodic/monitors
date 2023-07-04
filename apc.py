eb3fdbc0780ac1d617a60ad53d1ff57271bddff9
LuK <lucassneakers9@gmail.com>
2023-07-04 16:18:24 +0200

    apc.py testing

--- apc.py
+++ apc.py
@@ -1,8 +1,49 @@
+import time
 import requests
 from bs4 import BeautifulSoup
+from dhooks import Webhook, Embed
 
-url = "https://www.apc.fr/"
+HOOK_URL = 'https://discord.com/api/webhooks/1124627340681551902/QM1T_hoo_Ta73VJOuBk57_cfNscB5bTyVIMThM6uHRAeb9hl_j7xR5aUfjys1AOK2FAt'
+SITE_URL = 'https://www.apc.fr/soldes-30/men-sales/t-shirts-jeans.html'
 
-response = requests.get(url)
+def fetch_promotions():
+    page = requests.get(SITE_URL)
+    soup = BeautifulSoup(page.content, 'html.parser')
 
-print(response.status_code)
+    ul = soup.find('ul', class_='product-list')
+    items = ul.find_all('li', class_='product-item')
+
+    promotions = []
+    for item in items:
+        promotion = {
+            'link': item.find('a')['href'],
+            'image': item.find('img', class_='product-image entered loaded exited')['src'],
+            'name': item.find('span', class_='product-item-details').find('span', class_='product-name').text
+        }
+        promotions.append(promotion)
+
+    return promotions
+
+def send_to_discord(promotion):
+    hook = Webhook(HOOK_URL)
+    embed = Embed(
+        description=f"[{promotion['name']}]({promotion['link']})",
+        color=0x5CDBF0,
+        timestamp='now'  
+    )
+    embed.set_image(promotion['image'])
+    hook.send(embed=embed)
+
+def main():
+    seen_promotions = set()
+    while True:
+        promotions = fetch_promotions()
+        for promotion in promotions:
+            link = promotion['link']
+            if link not in seen_promotions:
+                send_to_discord(promotion)
+                seen_promotions.add(link)
+        time.sleep(60)  # Pause for 1 minute
+
+if __name__ == "__main__":
+    main()
---
+++ apc_test.py
@@ -0,0 +1,24 @@
+import requests
+from bs4 import BeautifulSoup
+
+url = "https://www.apc.fr/soldes-30/men-sales/t-shirts-jeans.html"
+
+response = requests.get(url)
+
+soup = BeautifulSoup(response.text, "html.parser")
+
+product_list = soup.find('ul', class_='product-list')
+products_items = product_list.find_all("li", class_="product-item")
+
+found_products = set()
+
+for product_item in products_items: 
+    product_name = product_item.find("span", class_="product-name")
+    
+    if product_name not in found_products:
+        found_products.add(product_name)
+        product_price = product_item.find("span", class_="price-wrapper")
+        product_link = product_item.find("a")
+
+print(found_products)
+
