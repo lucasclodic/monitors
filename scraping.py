fa4930b2ebc823cf83b5e12a090781f29aac8f92
LuK <lucassneakers9@gmail.com>
2023-07-04 21:46:38 +0200

    update api calls

--- scraping.py
+++ scraping.py
@@ -3,8 +3,13 @@ import json
 import time
 from datetime import datetime
 
-# Insérez votre URL d'API ici
-API_URL = "https://tickets.compagnie-oceane.fr/ws/?&func=liste_horaires&num_voyage=0&num_passage=0&server_celya=webb-ha3&_=1688483325057"
+# Insérez vos URLs d'API ici
+API_URLs = [
+    'https://tickets.compagnie-oceane.fr/ws/?&func=liste_horaires&num_voyage=0&num_passage=1&server_celya=webb-ha1&_=1688483324811',
+    'https://tickets.compagnie-oceane.fr/ws/?&func=liste_horaires&num_voyage=0&num_passage=1&server_celya=webb-ha3&_=1688483325475',
+    'https://tickets.compagnie-oceane.fr/ws/?&func=liste_horaires&num_voyage=0&num_passage=1&server_celya=webb-ha3&_=1688483325493',
+    'https://tickets.compagnie-oceane.fr/ws/?&func=liste_horaires&num_voyage=0&num_passage=1&server_celya=webb-ha3&_=1688483325501'
+]
 
 # Insérez votre URL de webhook Discord ici
 WEBHOOK_URL = 'https://discord.com/api/webhooks/1125808344758747237/2OsjYw2sla1S3I_u7DadCSNeZRZmtzP-qzOXm8cP2r3fy4QKnQ-xGht8ANvxv7R03W7X'
@@ -25,51 +30,52 @@ cookies = {
 first_run = True
 
 while True:
-    print("Interrogation de l'API...")
-    response = requests.get(API_URL, cookies=cookies)
-    data = response.json()
+    for API_URL in API_URLs:
+        print("Interrogation de l'API...")
+        response = requests.get(API_URL, cookies=cookies)
+        data = response.json()
 
-    for h in data['liste_horaires']:
-        # Combinaison de date_depart et texte_heure_depart pour une clé unique
-        unique_key = f"{h['date_depart']}-{h['texte_heure_depart']}"
-        date_depart = datetime.strptime(h['date_depart'], '%Y%m%d').strftime('%d/%m/%Y')
-        heure_depart = h['texte_heure_depart']
-        places_dispos = int(h['places_dispos'])
+        for h in data['liste_horaires']:
+            # Combinaison de date_depart et texte_heure_depart pour une clé unique
+            unique_key = f"{h['date_depart']}-{h['texte_heure_depart']}"
+            date_depart = datetime.strptime(h['date_depart'], '%Y%m%d').strftime('%d/%m/%Y')
+            heure_depart = h['texte_heure_depart']
+            places_dispos = int(h['places_dispos'])
 
-        print(f"Analyse de la traversée {unique_key}...")
+            print(f"Analyse de la traversée {unique_key}...")
 
-        if unique_key not in current_state:
-            # C'est la première fois que nous voyons cette croisière, donc nous l'ajoutons à notre état actuel
-            current_state[unique_key] = places_dispos
-            print(f"Première observation de la traversée {unique_key}. Nombre de places disponibles: {places_dispos}")
-        else:
-            # Nous avons déjà vu cette croisière, alors vérifions si le nombre de places a changé
-            old_places_dispos = current_state[unique_key]
-
-            if old_places_dispos != places_dispos and not first_run:
-                # Le nombre de places a changé, donc nous envoyons un webhook à Discord
-                discord_data = {
-                    'embeds': [{
-                        'title': "Des places viennent de se vendre",
-                        'color': 16711680,  # Couleur rouge
-                        'fields': [
-                            {'name': 'Date', 'value': date_depart, 'inline': False},
-                            {'name': 'Heure', 'value': heure_depart, 'inline': False},
-                            {'name': 'Nombre de places disponibles avant', 'value': str(old_places_dispos), 'inline': True},
-                            {'name': 'Nombre de places disponibles désormais', 'value': str(places_dispos), 'inline': True}
-                        ],
-                        'thumbnail': {'url': 'https://www.compagnie-oceane.fr/wp-content/themes/altibus/framework/website/assets/img/logo_compagnie_oceane.png'},
-                    }]
-                }
-                print(f"Envoi d'un webhook à Discord: {discord_data['embeds'][0]['title']}")
-                requests.post(WEBHOOK_URL, json=discord_data)
-
-                # Met à jour l'état actuel avec le nouveau nombre de places
+            if unique_key not in current_state:
+                # C'est la première fois que nous voyons cette croisière, donc nous l'ajoutons à notre état actuel
                 current_state[unique_key] = places_dispos
+                print(f"Première observation de la traversée {unique_key}. Nombre de places disponibles: {places_dispos}")
+            else:
+                # Nous avons déjà vu cette croisière, alors vérifions si le nombre de places a changé
+                old_places_dispos = current_state[unique_key]
 
-    # Mark first_run as False after the initial state recording
-    first_run = False
+                if old_places_dispos != places_dispos and not first_run:
+                    # Le nombre de places a changé, donc nous envoyons un webhook à Discord
+                    discord_data = {
+                        'embeds': [{
+                            'title': "Des places viennent de se vendre",
+                            'color': 16711935,
+                            'fields': [
+                                {'name': 'Date', 'value': date_depart, 'inline': False},
+                                {'name': 'Horaire', 'value': heure_depart, 'inline': False},
+                                {'name': 'Nombre de places disponibles avant', 'value': str(old_places_dispos), 'inline': True},
+                                {'name': 'Nombre de places disponibles désormais', 'value': str(places_dispos), 'inline': True}
+                            ],
+                            'thumbnail': {'url': 'https://www.compagnie-oceane.fr/wp-content/themes/altibus/framework/website/assets/img/logo_compagnie_oceane.png'},
+                        }]
+                    }
+                    print(f"Envoi d'un webhook à Discord: {discord_data['embeds'][0]['title']}")
+                    requests.post(WEBHOOK_URL, json=discord_data)
 
-    # Attend 60 secondes avant de vérifier à nouveau
-    print("Attente de 60 secondes avant la prochaine vérification...\n")
-    time.sleep(60)
+                    # Met à jour l'état actuel avec le nouveau nombre de places
+                    current_state[unique_key] = places_dispos
+
+        # Mark first_run as False after the initial state recording
+        first_run = False
+
+        # Attend 60 secondes avant de vérifier à nouveau
+        print("Attente de 60 secondes avant la prochaine vérification...\n")
+        time.sleep(60)
