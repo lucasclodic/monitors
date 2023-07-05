bf08309085a691800f24dff03b7c06557b1200b4
LuK <lucassneakers9@gmail.com>
2023-07-05 21:13:19 +0200

    updated apc.py

--- apc_test.py
+++ apc_test.py
@@ -1,24 +1,18 @@
 import requests
 from bs4 import BeautifulSoup
+import time
 
 url = "https://www.apc.fr/soldes-30/men-sales/t-shirts-jeans.html"
-
-response = requests.get(url)
-
-soup = BeautifulSoup(response.text, "html.parser")
-
-product_list = soup.find('ul', class_='product-list')
-products_items = product_list.find_all("li", class_="product-item")
+webhook_url = "https://discord.com/api/webhooks/1124627340681551902/QM1T_hoo_Ta73VJOuBk57_cfNscB5bTyVIMThM6uHRAeb9hl_j7xR5aUfjys1AOK2FAt"
 
 found_products = set()
 
-for product_item in products_items: 
-    product_name = product_item.find("span", class_="product-name")
-    
-    if product_name not in found_products:
-        found_products.add(product_name)
-        product_price = product_item.find("span", class_="price-wrapper")
-        product_link = product_item.find("a")
+while True: 
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, 'html.parser')
 
+    product_list = soup.find("ul", class_="product-list")
+    products_items = product_list.find_all("li", class_="product-item")
 
-
+    for product_item in products_items:
+        product_name = product_item.find()
